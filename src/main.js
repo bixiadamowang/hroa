3267960f78a4f52455a44172c44928ce73f4c210
hzc <384716313@qq.com>
2018-08-07 10:23:52 +0800

    完成首页

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
